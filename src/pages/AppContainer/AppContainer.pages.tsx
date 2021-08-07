8fe8211900675198bb5343d490468ee64fb20dfe
Krishna Ballabh <krishna.ballabh_cs19@gla.ac.in>
2021-08-07 13:46:27 +0530

    making navbar

--- src/pages/AppContainer/AppContainer.pages.tsx
+++ src/pages/AppContainer/AppContainer.pages.tsx
@@ -1,12 +1,14 @@
 import {FC, memo} from "react";
 import { Route, Switch } from "react-router-dom";
 import SideBar from "../../components/SideBar";
+import { User } from "../../models/User";
 import DashboardPages from "./Dashboard.pages";
 import RecordingsPages from "./Recordings.pages";
-interface Props{}
+interface Props{
+}
 
 const AppContainer: FC<Props> = (props) =>{
-    return(
+     return(
         <div className = "flex flex-row">
             <SideBar></SideBar>
         <Switch>
